22bac1c0df4a8dc12ed5a5d070f57633aa109257
Devpeace001 <peace.adebayo@verbenalogic.com>
2024-09-12 16:53:28 -0700

    demo app by peace

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -4,6 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+// Import modules
+// import { MaxNumberModule } from 'verbena-ui-library';
+// import { MinNumberModule } from 'verbena-ui-library';
+// import { RequiredInputModule } from 'verbena-ui-library';
+// import { EmailValidatorModule } from 'verbena-ui-library';
+// import { ConvertToDecimalModule } from 'verbena-ui-library';
+// import { ConvertToIntegerModule } from 'verbena-ui-library';
+// import { ConvertToStringModule } from 'verbena-ui-library';
+
+
 @NgModule({
   declarations: [
     AppComponent

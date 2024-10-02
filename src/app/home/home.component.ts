1208a4bd8db07ceb24c0c3ee99af746584986609
Devpeace001 <peace.adebayo@verbenalogic.com>
2024-10-02 10:28:14 -0700

    emitting input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  username: string = '';  // Variable to store the username input
+
+  onUsernameChange(value: string) {
+    this.username = value;  // Update the username variable with the input value
+    console.log('Username changed:', this.username);  // Log the username value for debugging
+  }
 
 }

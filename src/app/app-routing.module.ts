be3ac758b8ea1362c7f85a239b2c1d10c4487482
Fawole Ajibola Lukman <jibolafawole258@gmail.com>
2024-10-08 15:07:32 +0100

    quick fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,34 +4,14 @@ import { CardDataViewComponent } from './Components/card-data-view/cdv.component
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DropdownSampleComponent } from './views/dropdown-sample/dropdown-sample.component';
-import { ImageComponent, NotificationComponent, SvgComponent, TableFilterComponent, TooltipComponent } from '../../projects/verben-ng-ui/src/public-api';
+
 
 const routes: Routes = [
   {
     path: 'card-data-view',
     component: CardDataViewComponent,
   },
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  {
-    path: 'notifications',
-    component: NotificationComponent,
-  },
-  {
-    path: 'icons',
-    component: SvgComponent,
-  },
-  {
-    path: 'table-filter',
-    component: TableFilterComponent,
-  },
-  {
-    path: 'image',
-    component: ImageComponent,
-  },
-  {
-    path: 'tooltip',
-    component:TooltipComponent ,
-  }
+  { path: '', component: HomeComponent, pathMatch: 'full' }
 ];
 
 @NgModule({

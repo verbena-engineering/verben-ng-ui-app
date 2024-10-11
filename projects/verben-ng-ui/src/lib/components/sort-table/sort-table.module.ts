cc50bc6bbe01d14b881ef7e2013d3983d82a2cd8
Lloydpop <lloydperfect199@gmail.com>
2024-10-11 09:38:05 +0100

    updated

--- projects/verben-ng-ui/src/lib/components/sort-table/sort-table.module.ts
+++ projects/verben-ng-ui/src/lib/components/sort-table/sort-table.module.ts
@@ -3,10 +3,10 @@ import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common';
 import { SortTableComponent } from './sort-table.component';
 import { SvgModule } from '../svg/svg.module';
-import { VerbenaInputModule } from 'verben-ng-ui/src/public-api';
+
 @NgModule({
   declarations:[SortTableComponent],
-  imports: [FormsModule,CommonModule,CommonModule, FormsModule, SvgModule, VerbenaInputModule],
+  imports: [FormsModule,CommonModule,CommonModule, FormsModule, SvgModule],
   exports: [SortTableComponent]
 })
 export class SortTableModule {}

dc334316c901b81631ac44e172647f39c2c9a9cb
Davepea <dev.peacedave@gmail.com>
2024-10-13 15:04:11 -0700

    new

--- projects/verben-ng-ui/src/lib/verbena-button/verbena-button.component.ts
+++ projects/verben-ng-ui/src/lib/verbena-button/verbena-button.component.ts
@@ -19,7 +19,7 @@ export class VerbenaButtonComponent {
   @Input() fontSize: string = '14px';
   @Input() disable: boolean = false;
 
-  @Input() styleType: 'primary' | 'secondary' | 'danger' | 'small' | 'outline' = 'primary';  // Predefined styles
+  @Input() styleType: 'primary' | 'secondary' | 'danger' | 'small' | 'outline' | 'grey' | 'ylw-outline' = 'primary';  // Predefined styles
 
   @Input() svg: string = '';
   @Input() svgWidth: number = 20;
@@ -34,15 +34,15 @@ export class VerbenaButtonComponent {
           textColor: this.textColor || '#ffffff',
           border: this.border || 'none',
           borderRadius: this.borderRadius || '4px',
-          pd: this.pd || '8px 10px'
+          pd: this.pd || '10px 15px'
         };
       case 'secondary':
         return {
-          bgColor: this.bgColor || '#6c757d',
-          textColor: this.textColor || '#ffffff',
+          bgColor: this.bgColor || '#FFE681',
+          textColor: this.textColor || '#404040',
           border: this.border || 'none',
           borderRadius: this.borderRadius || '4px',
-          pd: this.pd || '8px 10px'
+          pd: this.pd || '10px 15px'
         };
       case 'danger':
         return {
@@ -60,13 +60,30 @@ export class VerbenaButtonComponent {
           borderRadius: this.borderRadius || '7px',
           pd: this.pd || '0px 10px'
         };
+
       case 'outline':
         return {
           bgColor: this.bgColor || 'none',
           textColor: this.textColor || '#000',
+          border: this.border || '1px solid #404040',
+          borderRadius: this.borderRadius || '5px',
+          pd: this.pd || '0px 8px'
+        };
+      case 'ylw-outline':
+        return {
+          bgColor: this.bgColor || '#FFFBEB',
+          textColor: this.textColor || '#404040',
+          border: this.border || '1px solid #FFE681',
+          borderRadius: this.borderRadius || '5px',
+          pd: this.pd || '10px 15px'
+        };
+      case 'grey':
+        return {
+          bgColor: this.bgColor || 'none',
+          textColor: this.textColor || '#D9D9D940',
           border: this.border || '2px solid grey',
           borderRadius: this.borderRadius || '4px',
-          pd: this.pd || '8px 20px'
+          pd: this.pd || '10px 15px'
         };
       default:
         return {};

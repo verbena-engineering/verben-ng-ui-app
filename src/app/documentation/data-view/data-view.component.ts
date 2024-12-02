6595d58356e5048603a3f32d48aed5a2462c551b
lloydpop <lloydperfect199@gmail.com>
2024-12-02 15:43:11 +0100

    updated

--- src/app/documentation/data-view/data-view.component.ts
+++ src/app/documentation/data-view/data-view.component.ts
@@ -143,7 +143,6 @@ export class DataViewComponent {
   }
 
   onStateChange(event: { key: string; value: boolean }): void {
-    console.log(`State changed for ${event.key}:`, event.value);
     if(event.key==='column'&& event.value===true){
       this.showColumn=event.value
     }

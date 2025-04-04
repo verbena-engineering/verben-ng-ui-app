34455872ee45edc274e36e68a8f0272e8cf7cd98
Davepea <dev.peacedave@gmail.com>
2025-04-04 02:24:34 -0700

    peace

---
+++ projects/verben-ng-ui/src/lib/components/number-input/number-input.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NumberInputComponent } from './number-input.component';
+
+@NgModule({
+  declarations: [NumberInputComponent],
+  imports: [CommonModule],
+  exports: [NumberInputComponent],
+})
+export class NumberInputModule {}
--- src/app/documentation/verbena-input-textarea/verbena-input-textarea.component.ts
+++ src/app/documentation/verbena-input-textarea/verbena-input-textarea.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-verbena-input-textarea',
@@ -10,8 +12,24 @@ export class VerbenaInputTextareaComponent {
   age: string = '';
   decimalValue: string = '';
 
+  form = new FormGroup({
+    quantity: new FormControl(5), // Initial value
+  });
+
+  onSubmit() {
+    console.log('Form Value:', this.form.value);
+  }
+
+  quantity: number = 1;
+
+  onValueChange(newValue: number) {
+    console.log('Updated Value:', newValue, this.quantity);
+  }
+
+
   submit() {
     console.log('Submitted Values:', {
+      quantity: this.quantity,
       email: this.email,
       age: this.age,
       decimalValue: this.decimalValue

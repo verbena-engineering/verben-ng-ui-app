18690742a36a0dc4472730fe0332bac21cba24c8
Davepea <dev.peacedave@gmail.com>
2025-05-14 04:55:56 -0700

    peace

--- src/app/documentation/data-table/data-table.module.ts
+++ src/app/documentation/data-table/data-table.module.ts
@@ -1,44 +1,44 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { DataTableRoutingModule } from './data-table-routing.module';
-import { DataTableComponent } from './data-table.component';
-// import { DataTableModule as LibDataTableModule } from '../../../../projects/verben-ng-ui/src/lib/components/data-table/data-table.module';
-import {
-  DataTableModule as LibDataTableModule,
-  CardModule,
-  DataExportModule,
-  DataExportService,
-  DropDownModule,
-  SvgModule,
-  TooltipModule,
-  VerbenaInputModule,
-  VerbenaTextareaModule,
-} from 'verben-ng-ui/src/public-api';
-import { TableExportComponent } from './table-export/table-export.component';
-import { DataFilterModule, DataXportModule, DataSortModule } from 'verben-ng-ui';
+// import { DataTableRoutingModule } from './data-table-routing.module';
+// import { DataTableComponent } from './data-table.component';
+// // import { DataTableModule as LibDataTableModule } from '../../../../projects/verben-ng-ui/src/lib/components/data-table/data-table.module';
+// import {
+//   DataTableModule as LibDataTableModule,
+//   CardModule,
+//   DataExportModule,
+//   DataExportService,
+//   DropDownModule,
+//   SvgModule,
+//   TooltipModule,
+//   VerbenaInputModule,
+//   VerbenaTextareaModule,
+// } from 'verben-ng-ui/src/public-api';
+// import { TableExportComponent } from './table-export/table-export.component';
+// import { DataFilterModule, DataXportModule, DataSortModule } from 'verben-ng-ui';
 
-@NgModule({
-  declarations: [DataTableComponent, TableExportComponent],
-  imports: [
-    CommonModule,
-    DataTableRoutingModule,
-    LibDataTableModule,
-    FormsModule,
-    ReactiveFormsModule,
-    DataExportModule,
-    DataXportModule,
-    DataFilterModule,
-    DataSortModule,
-    DropDownModule,
-    VerbenaInputModule,
-    CardModule,
-    SvgModule,
-    TooltipModule,
-    VerbenaTextareaModule,
-  ],
-  exports: [DataTableComponent],
-  providers: [DataExportService],
-})
-export class DataTableModule {}
+// @NgModule({
+//   declarations: [DataTableComponent, TableExportComponent],
+//   imports: [
+//     CommonModule,
+//     DataTableRoutingModule,
+//     LibDataTableModule,
+//     FormsModule,
+//     ReactiveFormsModule,
+//     DataExportModule,
+//     DataXportModule,
+//     DataFilterModule,
+//     DataSortModule,
+//     DropDownModule,
+//     VerbenaInputModule,
+//     CardModule,
+//     SvgModule,
+//     TooltipModule,
+//     VerbenaTextareaModule,
+//   ],
+//   exports: [DataTableComponent],
+//   providers: [DataExportService],
+// })
+// export class DataTableModule {}

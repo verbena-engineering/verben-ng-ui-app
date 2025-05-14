e0eb2e5d18e1ade3d7b51a4cb762a5d66004ec37
Babatunde Adenowo <mo.xodeeq@gmail.com>
2025-05-14 21:12:20 +0100

    Chnages to data-table

--- projects/verben-ng-ui/src/lib/components/data-table/data-table.types.ts
+++ projects/verben-ng-ui/src/lib/components/data-table/data-table.types.ts
@@ -19,6 +19,7 @@ export interface ColumnDefinition<T> {
   sortAction?: 'ASC' | 'DESC';
   formControlName?: string;
   groupName?: string;
+  validatorFn?: (value: T[keyof T]) => boolean;
 }
 
 // Define a type that extends T with a _key property
@@ -49,3 +50,45 @@ export interface FormGroupConfig<
 export type EditedData<T> = {
   [K in keyof T]?: T[K] extends object ? Partial<T[K]> : T[K];
 };
+
+// ====================================
+
+// export type ColumnDefinitionBase<T> = {
+//   id: string;
+//   header: string | ((context: any) => any);
+//   cellTemplate?: TemplateRef<any>;
+//   cellEditTemplate?: TemplateRef<any>;
+//   headerTemplate?: TemplateRef<any>;
+//   footerTemplate?: TemplateRef<any>;
+//   sortAction?: 'ASC' | 'DESC';
+//   formControlName?: string;
+//   groupName?: string;
+// };
+
+// // Helper type to create a column definition with a specific accessor key
+// export type ColumnDefinitionWithKey<
+//   T,
+//   K extends keyof T
+// > = ColumnDefinitionBase<T> & {
+//   accessorKey: K;
+//   accessorFn?: never;
+//   validatorFn?: (value: T[K]) => boolean;
+// };
+
+// export type ColumnDefinitionWithFn<T, R = any> = ColumnDefinitionBase<T> & {
+//   accessorKey?: never;
+//   accessorFn: (row: T) => R;
+//   validatorFn?: (value: R) => boolean;
+// };
+
+// export type ColumnDefinitionWithNeither<T> = ColumnDefinitionBase<T> & {
+//   accessorKey?: never;
+//   accessorFn?: never;
+//   validatorFn?: never;
+// };
+
+// // Union type that represents all possible column definition variants
+// export type ColumnDefinition<T> =
+//   | { [K in keyof T]: ColumnDefinitionWithKey<T, K> }[keyof T]
+//   | ColumnDefinitionWithFn<T>
+//   | ColumnDefinitionWithNeither<T>;
